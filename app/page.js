0db77f81a8fb0bb80c3be77aca7684df0b2c1873
majdishomali <majdi.shomali.1997@gmail.com>
2023-09-14 11:56:17 -0700

    ee

--- app/page.js
+++ app/page.js
@@ -4,35 +4,35 @@ import TopicCard from "@/components/cards/TopicCard";
 
 const NEXT_PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
-// const getTopics = async () => {
-//   try {
-//     const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/topics/0/[1,5]`, {
-//       // cache:"force-cache",//SSG getStaticSideProps
-//       // cache: "no-store", //SSR getServerSideProps
-//       next:{
-//        revalidate:20 //ISR===== ssr with sec
-//       },
-//     });
+const getTopics = async () => {
+  try {
+    const res = await fetch(`${NEXT_PUBLIC_API_URL}/api/topics/0/[1,5]`, {
+      // cache:"force-cache",//SSG getStaticSideProps
+      // cache: "no-store", //SSR getServerSideProps
+      next:{
+       revalidate:20 //ISR===== ssr with sec
+      },
+    });
 
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch topics");
-//     }
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
 
-//     return res.json();
-//   } catch (error) {
-//     console.log("Error loading topics: ", error);
-//   }
-// };
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+};
 
 const Home = async () => {
-  // const { topics,totalPages } = await getTopics();
+  const { topics,totalPages } = await getTopics();
 
   return (
     <>
-      {/* <HeroSection />
+      <HeroSection />
       <div className="bg-gray-100 flex flex-wrap justify-center items-center gap-5 h-screen">
         <TopicCard topics={topics} />
-      </div> */}
+      </div>
 
       
    
